2eb2a660bbdad7c7d255133e6c2f82dcf31081cc
ad.bme <adil.bouamrioune@gmail.com>
2022-10-26 13:29:20 +0200

    changement de couleur

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("aaaaaaaaarh")
 
 let toto = document.getElementById("jd")
+
 console.log(toto)
 toto.addEventListener("click", function() {
     console.log("j'ai")
@@ -14,4 +15,18 @@ toto.addEventListener("click", function() {
     
 })
 
+let toto2 = document.getElementById("jd2")
+
+console.log(toto2)
+toto2.addEventListener("input", function() {
+    console.log("j'ai")
+    let articles = document.getElementsByTagName("h1")
+
+    for (const article of articles){
+    article.style.backgroundColor = "#" +(Math.random()*0xFFFFFF<<0).toString(16);
+    }
+    
+
+    
+})
 
