ed083b7b20315970f58729f1ec58b1838c6088f9
ad.bme <adil.bouamrioune@gmail.com>
2022-10-26 09:49:27 +0200

    calculateur js

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
 console.log("aaaaaaaarh")
+
+let adil = {
+  name: "Adil"
+
+}
+console.log(adil.name)
+
+const t = 0
+
+let ta = [1, 6, 4] 
+
+
+function average (tableau) {
+    let total = 0
+
+    for (const note of tableau){
+        total+=note   
+    }
+    
+    return total/tableau.length 
+}
+
+
+console.log(average(ta))
